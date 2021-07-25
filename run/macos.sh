7879db7bd3bbd45f4ef93bae3fe1d784931fc5ab
Chatyusha <hirounicat76@gmail.com>
2021-07-25 18:32:12 +0900

    limitmemory

--- run/macos.sh
+++ run/macos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ARCH="$(uname -m)"
-sudo docker run -it -w /root --name LinuxSummer2021 unicatflower/standard-ubuntu-environment:$ARCH
+sudo docker run -it -w /root --memory=256mb --name LinuxSummer2021 unicatflower/standard-ubuntu-environment:$ARCH
--- run/win64.bat
+++ run/win64.bat
@@ -1,3 +1,3 @@
 setlocal
 
-sudo docker run -it -w /root --name LinuxSummer2021 unicatflower/standard-ubuntu-environment:x86_64
+sudo docker run -it -w /root --memory=256mb --name LinuxSummer2021 unicatflower/standard-ubuntu-environment:x86_64
